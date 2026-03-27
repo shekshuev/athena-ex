059a0ebf7a4e8eb7fa180bff53be98f2d7e0539f
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2026-03-28 00:59:07 +0300

    upd: acl

--- lib/athena_web/live/studio_live/library.ex
+++ lib/athena_web/live/studio_live/library.ex
@@ -13,7 +13,7 @@ defmodule AthenaWeb.StudioLive.Library do
   alias Athena.Identity
   alias AthenaWeb.StudioLive.LibraryFormComponent
 
-  on_mount {AthenaWeb.Hooks.Permission, "library.read"}
+  on_mount {AthenaWeb.Hooks.Permission, "courses.read"}
 
   @doc """
   Initializes the LiveView, setting up the library blocks stream.
