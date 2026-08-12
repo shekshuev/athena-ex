985ae2e741d56abac25bd2a427ff4e839c3b9e98
kiruxashafr <kirshafranov@gmail.com>
2026-08-12 23:05:41 +0300

    fix: plug test

--- test/athena_web/plugs/fetch_current_user_test.exs
+++ test/athena_web/plugs/fetch_current_user_test.exs
@@ -1,20 +1,22 @@
 defmodule AthenaWeb.Plugs.FetchCurrentUserTest do
   use ExUnit.Case, async: true
-  use Plug.Test
+
+  import Plug.Conn
+  import Plug.Test
 
   alias AthenaWeb.Plugs.FetchCurrentUser
 
   @session_options Plug.Session.init(
-                     store: :cookie,
-                     key: "_app",
-                     signing_salt: "secret_salt"
-                   )
+    store: :cookie,
+    key: "_app",
+    signing_salt: "secret_salt"
+  )
 
   test "clears :account_id from session when account does not exist" do
     non_existent_id = -1
 
     conn =
-      build_conn()
+      conn(:get, "/")
       |> Plug.Session.call(@session_options)
       |> fetch_session()
       |> put_session(:account_id, non_existent_id)
@@ -22,7 +24,6 @@ defmodule AthenaWeb.Plugs.FetchCurrentUserTest do
     conn = FetchCurrentUser.call(conn, FetchCurrentUser.init([]))
 
     assert conn.assigns.current_user == nil
-
     assert get_session(conn, :account_id) == nil
   end
 end
